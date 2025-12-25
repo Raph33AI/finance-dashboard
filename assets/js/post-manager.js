4a9f66c09ef6627988cb84a3ff61265355430199
Raph33AI <raphnardone@gmail.com>
2025-12-25 11:31:56 +0100

    New

--- assets/js/post-manager.js
+++ assets/js/post-manager.js
@@ -787,7 +787,7 @@ class PostManager {
                             <!-- X -->
                             <a href="#" class="share-platform-btn twitter" data-platform="twitter">
                                 <div class="platform-icon">
-                                    <i class="fab fa-x"></i>
+                                    <i class="fab fa-twitter"></i>
                                 </div>
                                 <span>X</span>
                             </a>
